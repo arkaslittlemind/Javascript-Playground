d20b45816b4c1efb68be8cfde6b6980315139ffc
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-12 03:07:57 +0530

    master

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 //console.log('Hello World!');
 
-let name = 'Arka';
-console.log(name);
+//let name = 'Arka';
+//console.log(name);
 
 const interestRate = 0.3;
 interestRate = 1;
 console.log(interestRate);
+
+// Primitive Types
+let name = 'Arka'; // String Literal
+let age = 22; // Number Literal
+let isApproved = true; // Boolean Literal
