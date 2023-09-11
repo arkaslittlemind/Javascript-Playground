77c9774f43baa09e9c78965b1918b62d7e4f9068
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-12 03:33:24 +0530

    master

---
+++ functions.js
@@ -0,0 +1,6 @@
+function greet(name, lastName) {
+  console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('Arka', 'Mojumder'); // Calling the function
+greet('Shristi', 'Pradhan'); // Calling the function
