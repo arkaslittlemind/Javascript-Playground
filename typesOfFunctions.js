5288080622c649575b3f92541f6880cbadadbe96
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-12 03:37:50 +0530

    master

---
+++ typesOfFunctions.js
@@ -0,0 +1,13 @@
+// Performing a task
+function greet(name, lastName) {
+  console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('Arka', 'Mojumder'); // Calling the function
+
+function square(number) {
+  return number * number;
+}
+
+square(2); // Calling the function
+console.log(square(2));
