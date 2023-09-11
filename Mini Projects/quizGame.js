7facaf1c914d56a559aaa7bf5708067e417d35b9
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-12 04:10:55 +0530

    master

--- Mini Projects/quizGame.js
+++ Mini Projects/quizGame.js
@@ -1,5 +1,19 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')();
 
-const value = prompt('Enter something: ');
-console.log('You entered: ' + value);
+console.log('Welcome to the Quiz Game!');
+
+const answer1 = prompt('What is the brain of the computer?');
+const correctAnswer1 = 'CPU';
+
+if(answer1 === correctAnswer1) {
+  console.log('Correct!');
+} else {
+  console.log('Incorrect!');
+}
+
+
+
+
+
+
 
