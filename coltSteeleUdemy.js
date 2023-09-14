a520c704b8fed6c5e0807e6ef3fe64040ee4c129
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-14 20:54:28 +0530

    master

--- coltSteeleUdemy.js
+++ coltSteeleUdemy.js
@@ -12,3 +12,22 @@ parseInt('24')
 parseFloat('24.5') 
 
 console.log(parseFloat('i ate 3 apples'))  
+
+// Double equals vs triple equals
+
+// 1. Double equals does type coercion
+// 2. Double equals checks for equality of value, but not equality of type
+// 3. It coerces(converts) both values to the same type and then compares them.
+// 4. This can lead to some unexpected results!
+
+console.log(4 == 4) ; // true
+console.log(4 == '4'); // true ie; type coercion (== evaluates to true but in reality it is not true)
+
+
+// 1. Triple equals does not do type coercion
+// 2. Triple equals checks for equality of value AND type
+// 3. If the types are different, it returns false
+// 4. If the types are the same, it checks the values
+
+console.log(4 === 4); // true
+console.log(4 === '4'); // false ie; type coercion (=== evaluates to false because in reality it is false, one is a number and the other is a string)
