7af6f1b5069bd02c559cd1ae70ce65b2dfbd5488
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-02-29 13:09:53 +0530

    Filter O/P Question

---
+++ Map-Filter-Reduce/Output-Questions/Q2.js
@@ -0,0 +1,15 @@
+//Question 2- Return details of those who scored more than 60
+
+let students = [
+    { name: 'Arka', age: 23, marks: 80 },
+    { name: 'Shristi', age: 23, marks: 69 },
+    { name: 'Anik', age: 24, marks: 90 },
+    { name: 'Aritra', age: 24, marks: 35 },
+    { name: 'Debarpan', age: 24, marks: 55 }
+];
+
+const studentsReduce = students.filter((student) => 
+    student.marks > 60
+);
+
+console.log(studentsReduce)
