dd36437ea3a004bb95553bd0bf2be051fa5b1f51
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-02-29 14:44:47 +0530

    reduce array method

--- Map-Filter-Reduce/reduce.js
+++ Map-Filter-Reduce/reduce.js
@@ -1,5 +1,6 @@
 // Reduce array operation-
-// The reduce() method applies a function against an accumulator and each element in the array (from left to right) to reduce it to a single value.
+// The reduce() method applies a
+// function against an accumulator and each element in the array (from left to right) to reduce it to a single value.
 
 const nums = [1, 2, 3, 4, 5];
 
