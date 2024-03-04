0be719e54b478cd41f70b9514b020f275776d804
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-03-04 20:59:02 +0530

    Data Types

---
+++ Basics/DataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // Using strict mode ie; ES6(ECMA Script 6) standards
+
+console.log("Data Types in JavaScript");
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+let name = "Arka"
+let age = 23
+let isApproved = true
+
+// number
+// bigint
+// string 
+// boolean
+// undefined => variable declared but not assigned any value
+// null => standalone value
+// symbol => unique
+
+
+// object
+
+console.table([name, age, isApproved])
+console.log(typeof name) // string
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
