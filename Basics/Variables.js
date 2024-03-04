c9f1b4c036bd51e45879e5258407fae2b9acb5ff
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-03-04 20:31:46 +0530

    nodemon added

--- Basics/Variables.js
+++ Basics/Variables.js
@@ -2,8 +2,19 @@ const accountId = 144553;
 let accountName = "Arkadipta Mojumder";
 var accountPassword = "12345";
 accountCity = "Vellore";
+let accountState;
 
+// accountId = 2; // not allowed
 
-console.log(accountId);
+accountName = "Shristi Pradhan";
+accountPassword = "67890";
+accountCity = "Kolkata";
 
+// console.log(accountId);
 
+/* 
+Prefer let over var because of issue in 
+block scope and functional scope
+*/
+
+console.table([accountName, accountPassword, accountCity, accountId, accountState]);
