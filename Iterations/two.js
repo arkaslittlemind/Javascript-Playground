58ef7265a3683328d76e4a9d36a4d75617f166ce
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-03-08 00:14:31 +0530

    while and do-while

---
+++ Iterations/two.js
@@ -0,0 +1,21 @@
+let index = 10;
+while (index <= 20) {
+//   console.log(`Value of index is: ${index}`);
+  index += 2;
+}
+
+let myArray = ["flash", "batman", "superman"];
+
+let arr = 0;
+
+while (arr < myArray.length) {
+    // console.log(`Value of arr is: ${myArray[arr]}`);
+    arr = arr + 1;
+}
+
+let score = 11
+
+do {
+    console.log(`Value of score is: ${score}`);
+    score++;
+} while (score <= 10);
