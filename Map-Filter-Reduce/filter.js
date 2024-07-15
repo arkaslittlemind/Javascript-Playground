e35ff7a8c878115cdd2a09d7999fb9d10e7dd371
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-07-15 21:26:46 +0530

    filter revision

--- Map-Filter-Reduce/filter.js
+++ Map-Filter-Reduce/filter.js
@@ -3,8 +3,28 @@
 
 const nums = [1, 2, 3, 4, 5];
 
-const numsGreaterThan3 = nums.filter((num) => {
-    return num > 3;
+// const numsGreaterThan3 = nums.filter((num) => {
+//     return num > 3;
+// })
+
+// console.log(numsGreaterThan3); // [4, 5]
+
+// Given an input array of numbers, return a new array containing only even numbers
+
+// approach 1
+const newArray = [];
+for(let i = 0; i < nums.length; i++) {
+    if(nums[i] % 2 === 0) {
+        newArray.push(nums[i]);
+    }
+}
+
+// console.log(newArray);
+
+// approach 2 - clean way
+
+const evenNumbers = nums.filter(function(arr) {
+    return arr % 2 === 0;
 })
 
-console.log(numsGreaterThan3); // [4, 5]
+console.log(evenNumbers); // [2, 4]
